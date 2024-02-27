df84c7d56e0d34f18a8988ab5ccecef81aebc2b4
Marco Pazol <donjulionlimon@gmail.com>
2024-02-26 18:45:58 -0600

    KG-LB script working with succesfully, but rest to implement more samples within training dataset

--- Tensorflow/kg_to_pounds_converter.py
+++ Tensorflow/kg_to_pounds_converter.py
@@ -1,17 +1,12 @@
 import numpy as np
 import keras
 from keras.models import save_model, load_model
+import matplotlib.pyplot as plt
 
 """Datasets"""
 #Data arrays
-pounds = []
 kilogrames = np.array([1, 2, 5, 10, 17, 20, 50, 85, 100, 104, 177, 820, 1000, 1289, 5270])
-
-for kg in kilogrames:
-    pound = kg * 2.20462
-    pounds.append(pound)
-    
-pounds = np.array(pounds)
+pounds = np.array([2.20462, 4.40925, 11.0231, 22.0462, 37.47854, 44.0924, 110.231, 187.3927, 220.462, 229.28048, 390.21774, 1807.7884, 2204.62, 2841.75518, 11618.3474])
 
 #Loading dataset
 x_train = kilogrames
@@ -23,34 +18,37 @@ try:
 except:
     #Architecture
     model = keras.models.Sequential([
-        keras.layers.Dense(units=1, input_shape = [1], activation="relu"),
-        keras.layers.Dense(1, activation="linear")
+        keras.layers.Dense(units=1, input_shape = [1])
     ])
+# #Compiling
+# model.compile(optimizer = keras.optimizers.Adam(0.5), loss = "mean_squared_error")
 
-#Compiling
-model.compile(optimizer = "adam", loss = "mean_squared_error")
+# #Training
+# epochs_hist = model.fit(x_train, y_train, epochs=300)
 
-#Training
-epochs_hist = model.fit(x_train, y_train, epochs=100)
+# #Saving model
+# #model.save("C:/Users/raven/OneDrive/Escritorio/Coding/Python/Practices/AI_Practice/Trained_Models/kg_lb_model.h5")
 
-#Saving model
-model.save("C:/Users/raven/OneDrive/Escritorio/Coding/Python/Practices/AI_Practice/Trained_Models/kg_lb_model.h5")
+# print(epochs_hist.history.keys)
+# print(model.summary())
+# print(kilogrames)
+# print(pounds)
 
-print(epochs_hist.history.keys)
-print(model.summary())
-print(kilogrames)
-print(pounds)
+# """Training graphic for model accuracy progress during training"""
+# plt.plot(epochs_hist.history["loss"])
+# plt.xlabel("Epoch")
+# plt.ylabel("Loss")
+# plt.title("Lossing progress during model training")
+# plt.show()
 
-def predict(model:object, kg_input:float, kilogrames_array:object, pounds_array:object):
+def predict(model:object, kg_input:float):
     """
     DOCSTRING:
-        This method calculate if the transformed kg to lb given value is correct or not, if it is correct so the value will be saved as new data in the kg and lb datasets, if the result is incorrect then it will be discarted.
+        This method calculate if the transformed kg to lb given value is correct or not.
     
     Attributes:
         model:object,
         kg_input:float,
-        kilogrames_array:object,
-        pounds_array:object
      
     Return:
         result:str
@@ -58,29 +56,28 @@ def predict(model:object, kg_input:float, kilogrames_array:object, pounds_array:
     
     model = model
     kg_input = kg_input
-    kilogrames = kilogrames_array
-    pounds = pounds_array
 
-    predicted_pounds = model.predict([kg_input])
+    predicted_pounds = model.predict(np.array([kg_input]))
     real_pounds = kg_input * 2.20462
     
-    if predicted_pounds == real_pounds:
-        kg_added = np.array([kg_input])
-        lb_added = np.array([predicted_pounds])
-        kilogrames = np.concatenate(kilogrames, kg_added)
-        pounds = np.concatenate(pounds, lb_added)
+    threshold = 0.06
+    if abs(predicted_pounds - real_pounds) <= threshold:
         succesfully:bool = True
-        x_train = kilogrames
-        y_train = pounds
-        model.fit(x_train, y_train, epochs=10)
-        model.save("C:/Users/raven/OneDrive/Escritorio/Coding/Python/Practices/AI_Practice/Trained_Models/kg_lb_model.h5") 
+        print(abs(predicted_pounds - real_pounds), threshold)
     else:
         succesfully:bool = False
+        print(abs(predicted_pounds - real_pounds), threshold)
     
     result:str = f"Kg input:{kg_input}\nPredicted Lb:{predicted_pounds}\nReal Lb:{real_pounds}\nSuccesfully:{succesfully}"
-    
     return result
     
-print(predict(model, 96, kilogrames, pounds))
-print(predict(model, 105, kilogrames, pounds))
-print(predict(model, 90, kilogrames, pounds))
+print(predict(model, 96))
+print(predict(model, 105))
+print(predict(model, 90))
+print(predict(model, 260))
+print(predict(model, 400))
+print(predict(model, 25))
+print(predict(model, 10))
+print(predict(model, 28))
+print(predict(model, 30))
+print(predict(model, 5200))
